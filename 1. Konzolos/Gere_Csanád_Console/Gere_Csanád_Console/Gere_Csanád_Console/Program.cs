8710e6349b0b9aa6bd2871fb50ac6cf9d661667e
csan <102951810+csanszy@users.noreply.github.com>
2025-04-07 22:48:13 +0200

    Add files via upload

---
+++ 1. Konzolos/Gere_Csanád_Console/Gere_Csanád_Console/Gere_Csanád_Console/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+
+namespace Gere_Csanád_Console
+{
+    internal class Program
+    {
+        List<Konyvtar> diak;
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.diak = Konyvtar.ReadFile();
+            program.HarmadikFeladat();
+            program.NegyedikFeladat();
+            program.ÖtödikFeladat();
+            program.HatodikFeladat();
+            program.HetedikFeladat();
+        }
+
+        void HarmadikFeladat()
+        {
+            Console.WriteLine($"3. feladat: A forrásfájlban {diak.Count()} diák adatai találhatók.");
+        }
+
+        void NegyedikFeladat()
+        {
+            Console.WriteLine($"4. feladat:");
+            diak.GroupBy(a => a.verseny).OrderByDescending(g => g.Key).ToList().ForEach(g => Console.WriteLine($"\t{g.Key} {g.Count()}"));
+        }
+
+        void ÖtödikFeladat()
+        {
+            Console.WriteLine($"5. feladat:");
+            diak.Where(a => a.szazalek >= 90).ToList().OrderByDescending(a => a.szazalek).ToList().ForEach(a => Console.WriteLine($"\t{a.name}, {a.iskola}, {a.szazalek}"));
+        }
+
+        void HatodikFeladat()
+        {
+            Console.WriteLine($"6. feladat: A Neumann János Technikum diákjainak átlagos eredménye: {String.Format("{0:0}", Math.Round(diak.Where(a => a.iskola == "Neumann János Technikum").Average(a => a.szazalek), 1))}");
+        }
+
+        void HetedikFeladat()
+        {
+            Console.WriteLine($"7. feladat: Legjobb eredmenyt elérő diák kiadott könyv: {diak.OrderByDescending(a => a.szazalek).First().name}, {diak.OrderByDescending(a => a.szazalek).Last().szazalek}");
+        }
+    }
+}
---
+++ 1. Konzolos/Gere_Csanád_Console/Gere_Csanád_Console/Gere_Csanád_Console/konyvtar.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gere_Csanád_Console
+{
+    public class Konyvtar
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string iskola/*szerzo*/ { get; set; }
+        public string datum/*kiadas_ev*/ { get; set; }
+        public string verseny /* konyvtar */ { get; set; }
+        public int szazalek { get; set; }
+
+        public Konyvtar(string[] sor)
+        {
+            this.id = int.Parse(sor[0]);
+            this.name = sor[1];
+            this.iskola = sor[2];
+            this.datum = sor[3];
+            this.verseny = sor[4];
+            this.szazalek = int.Parse(sor[5]);
+        }
+
+        public static List<Konyvtar> ReadFile()
+        {
+            List<Konyvtar> konyv = new List<Konyvtar>();
+            File.ReadAllLines("students.csv").ToList().Skip(1).ToList().ForEach(l =>
+            {
+                konyv.Add(new Konyvtar(l.Split(",")));
+            });
+            return konyv;
+        }
+    }
+}
